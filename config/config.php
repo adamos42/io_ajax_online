295ba1a082e0b117ebcd880d2a1d0c3daca608bc
Adam Liszkai <adamos42@gmail.com>
2015-02-22 12:57:58 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,18 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
-$config['module']['ajax_online'] = array
+$config['module']['io_online'] = array
 (
-    'module' => "Ajax_online",
+    'module' => "io_online",
     'name' => "Ajax Online Visitor counter",
     'description' => "Ajax module what count the online visitors",
-    'author' => "adamos42",
-    'version' => "0.1",
- 
-    // 'uri' should be the module's folder in lowercase.
-    // From 1.0.3, it is not mandatory to set 'uri'.
-    'uri' => 'ajax_online',
+    'author' => "Adam Liszkai <contact@liszkaiadam.hu>",
+    'version' => "0.1.0",
+    
+    'uri' => 'io_online',
     'has_admin'=> FALSE,
     'has_frontend'=> TRUE,
 );
  
-return $config['module']['ajax_online'];
+return $config['module']['io_online'];
